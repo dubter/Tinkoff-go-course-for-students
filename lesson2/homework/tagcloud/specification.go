80bbe300d5169fc84ea7a54eb134696c93aca389
dubter <dubenkov.rs@phystech.edu>
2023-03-01 02:48:21 +0300

    task2

--- lesson2/homework/tagcloud/specification.go
+++ lesson2/homework/tagcloud/specification.go
@@ -1,8 +1,10 @@
 package tagcloud
 
+import "sort"
+
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
-	// TODO: add fields if necessary
+	Stat map[string]int
 }
 
 // TagStat represents statistics regarding single tag
@@ -13,16 +15,20 @@ type TagStat struct {
 
 // New should create a valid TagCloud instance
 // TODO: You decide whether this function should return a pointer or a value
-func New() TagCloud {
-	// TODO: Implement this
-	return TagCloud{}
+func New() *TagCloud {
+	Stat := map[string]int{}
+	return &TagCloud{Stat}
 }
 
 // AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
 // thread-safety is not needed
 // TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) AddTag(tag string) {
-	// TODO: Implement this
+func (tagCloud TagCloud) AddTag(tag string) {
+	if _, hasValue := tagCloud.Stat[tag]; hasValue {
+		tagCloud.Stat[tag] += 1
+	} else {
+		tagCloud.Stat[tag] = 1
+	}
 }
 
 // TopN should return top N most frequent tags ordered in descending order by occurrence count
@@ -31,7 +37,17 @@ func (TagCloud) AddTag(tag string) {
 // thread-safety is not needed
 // there are no restrictions on time complexity
 // TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) TopN(n int) []TagStat {
-	// TODO: Implement this
-	return nil
+func (tagCloud TagCloud) TopN(n int) []TagStat {
+	topN := make([]TagStat, 0, n)
+	for tag, count := range tagCloud.Stat {
+		node := TagStat{tag, count}
+		topN = append(topN, node)
+	}
+	sort.SliceStable(topN, func(i, j int) bool {
+		return topN[i].OccurrenceCount > topN[j].OccurrenceCount
+	})
+	if n >= len(topN) {
+		return topN
+	}
+	return topN[:n]
 }
