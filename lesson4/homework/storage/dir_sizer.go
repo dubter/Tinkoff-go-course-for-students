ea3e94e04d77e62baf63aad56acd92c7ad687eea
dubter <dubenkov.rs@phystech.edu>
2023-03-15 03:14:25 +0300

    linter

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"sync"
 )
 
 // Result represents the Size function result
@@ -22,16 +23,88 @@ type DirSizer interface {
 type sizer struct {
 	// maxWorkersCount number of workers for asynchronous run
 	maxWorkersCount int
-
-	// TODO: add other fields as you wish
+	wg              sync.WaitGroup
+	commonSize      int64
+	commonCount     int64
+	err             error
 }
 
 // NewSizer returns new DirSizer instance
 func NewSizer() DirSizer {
-	return &sizer{}
+	sizerCustom := sizer{}
+	sizerCustom.maxWorkersCount = 10
+	return &sizerCustom
 }
 
 func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
-	// TODO: implement this
-	return Result{}, nil
+	dirs, files, err := d.Ls(ctx)
+	if err != nil {
+		a.err = err
+	}
+
+	for _, file := range files {
+		sizeFile, errReadStat := file.Stat(ctx)
+		if errReadStat != nil {
+			a.err = errReadStat
+		}
+		a.commonSize += sizeFile
+		a.commonCount++
+	}
+
+	for _, dir := range dirs {
+		if a.maxWorkersCount > 0 {
+			a.maxWorkersCount--
+			a.wg.Add(1)
+			dir := dir
+			go func() {
+				defer a.wg.Done()
+				defer func() {
+					a.maxWorkersCount++
+				}()
+				a.SizeUsingGorutine(ctx, dir)
+			}()
+		} else {
+			_, _ = a.Size(ctx, dir)
+		}
+	}
+
+	a.wg.Wait()
+
+	if a.err != nil {
+		return Result{0, 0}, a.err
+	}
+	return Result{a.commonSize, a.commonCount}, nil
+}
+
+func (a *sizer) SizeUsingGorutine(ctx context.Context, d Dir) {
+	dirs, files, err := d.Ls(ctx)
+	if err != nil {
+		a.err = err
+	}
+
+	for _, file := range files {
+		sizeFile, errReadStat := file.Stat(ctx)
+		if errReadStat != nil {
+			a.err = errReadStat
+		}
+		a.commonSize += sizeFile
+		a.commonCount++
+	}
+
+	for _, dir := range dirs {
+		if a.maxWorkersCount > 0 {
+			a.maxWorkersCount--
+			a.wg.Add(1)
+			dir := dir
+			go func() {
+				defer a.wg.Done()
+				defer func() {
+					a.maxWorkersCount++
+				}()
+				a.SizeUsingGorutine(ctx, dir)
+			}()
+		} else {
+			_, _ = a.Size(ctx, dir)
+		}
+	}
 }
